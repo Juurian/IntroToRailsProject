52fe86be75dfae73eb417a640c99f6916cc97535
jaypang0422 <kpang@academic.rrc.ca>
2023-10-09 00:51:02 -0500

    added a partial - _navbar.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
+<<<<<<< Updated upstream
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
+=======
+Rails.application.routes.draw do
+  root "menus#index"
+
+  resources :abouts, only: [:index]
+  resources :menus, only: [:index]
+  resources :deals, only: [:index]
+  resources :good_deals, only: [:index]
+  resources :game_summaries, only: [:index]
+end
+>>>>>>> Stashed changes

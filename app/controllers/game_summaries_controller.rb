7d3b55f4fa9c543af71196d6b4353ecf1ea12d00
Kwanwai Pang <kpang@academic.rrc.ca>
2023-10-09 00:21:59 -0500

    a search function added to game_summaries page

--- app/controllers/game_summaries_controller.rb
+++ app/controllers/game_summaries_controller.rb
@@ -1,5 +1,11 @@
 class GameSummariesController < ApplicationController
   def index
-    @game_summaries = GameSummary.paginate(page: params[:page], per_page: 20)
+    @game_summaries = if params[:search].present?
+                        GameSummary.where("summary_game_title LIKE ?", "%#{params[:search]}%").paginate(
+                          page: params[:page], per_page: 20
+                        )
+                      else
+                        GameSummary.paginate(page: params[:page], per_page: 20)
+                      end
   end
 end

a1575d315d9d885dcf581c5a717168b8c57e97fc
Kwanwai Pang <kpang@academic.rrc.ca>
2023-10-11 13:38:14 -0500

    free games page: added search and pagination, no bootstrap used due no thumbnail in dataset

--- app/controllers/free_games_controller.rb
+++ app/controllers/free_games_controller.rb
@@ -1,6 +1,16 @@
 class FreeGamesController < ApplicationController
+  # def index
+  #   # Fetch the FreeGame records from the database
+  #   @free_games = FreeGame.all
+  # end
+
   def index
-    # Fetch the FreeGame records from the database
     @free_games = FreeGame.all
+
+    # Search functionality
+    @free_games = @free_games.where("title LIKE ?", "%#{params[:search]}%") if params[:search]
+
+    # Pagination
+    @free_games = @free_games.paginate(page: params[:page], per_page: 10)
   end
 end

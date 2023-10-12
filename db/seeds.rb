c7981ceceaa0aed33c1a05ab6c2ad1b64ad9ae9a
jmacaraeg <jmacaraeg@academic.rrc.ca>
2023-10-12 18:03:41 -0500

    fixed seeds and db

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,33 +3,41 @@ require "httparty"
 require "csv"
 require "active_record"
 
-# # Delete all exsiting data from game_summaries
-# GameSummary.destroy_all
+########################
+##  Game Summary Seed ##
+########################
 
-# # Specify the file path to your CSV file
-# csv_file = Rails.root.join("db/csv_data/game_summaries_full.csv")
+# Delete all exsiting data from game_summaries
+GameSummary.destroy_all
 
-# # Read and create records from the CSV file
-# CSV.foreach(csv_file, headers: true) do |row|
-#   GameSummary.create!(
-#     summary_game_title: row["Title"],
-#     game_summary:       row["Summary"]
-#   )
-# end
+# Specify the file path to your CSV file
+csv_file = Rails.root.join("db/csv_data/game_summaries_full.csv")
+
+# Read and create records from the CSV file
+CSV.foreach(csv_file, headers: true) do |row|
+  GameSummary.create!(
+    summary_game_title: row["Title"],
+    game_summary:       row["Summary"]
+  )
+end
 
 # Delete all exsiting data from cheap_games
 CheapGame.destroy_all
 
-# # Specify the file path to your CSV file
-# csv_file = Rails.root.join("db/csv_data/game_summaries.csv")
+# Specify the file path to your CSV file
+csv_file = Rails.root.join("db/csv_data/game_summaries_full.csv")
 
-# # Read and create records from the CSV file
-# CSV.foreach(csv_file, headers: true) do |row|
-#   GameSummary.create!(
-#     summary_game_title: row["Title"],
-#     game_summary:       row["Summary"]
-#   )
-# end
+# Read and create records from the CSV file
+CSV.foreach(csv_file, headers: true) do |row|
+  GameSummary.create!(
+    summary_game_title: row["Title"],
+    game_summary:       row["Summary"]
+  )
+end
+
+########################
+##   Giveaway Seed    ##
+########################
 
 # Define a method to fetch data from the API
 def fetch_giveaways
@@ -59,26 +67,55 @@ giveaways_data.each do |giveaway|
     status:       giveaway["status"]
   )
 end
+
+########################
+##  Cheap Games Seed  ##
+########################
+
 # Fetch data from the API
 response2 = HTTParty.get("https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15")
 deals = JSON.parse(response2.body)
 
-# Loop through the API data and create CheapGame records
 deals.each do |deal|
   # Find the GameSummary based on the "summary_game_title" attribute
   game_summary = GameSummary.find_by(summary_game_title: deal["title"])
-  game_summary_id = game_summary.id if game_summary
 
-  # Create the CheapGame record and associate it with the GameSummary
-  CheapGame.create(
-    game_summary_id:,
-    title:                deal["title"],
-    metacritic_link:      deal["metacriticLink"],
-    metacritic_score:     deal["metacriticScore"],
-    steam_rating_text:    deal["steamRatingText"],
-    steam_rating_percent: deal["steamRatingPercent"],
-    steam_rating_count:   deal["steamRatingCount"],
-    steam_app_id:         deal["steamAppID"],
-    thumbnail:            deal["thumb"]
-  )
+  if game_summary
+    CheapGame.create(
+      game_summary_id:    game_summary.id,
+      title:              deal["title"],
+      metacritic_link:    deal["metacriticLink"],
+      metacritic_score:   deal["metacriticScore"],
+      steam_rating_text:  deal["steamRatingText"],
+      steam_rating_percent: deal["steamRatingPercent"],
+      steam_rating_count: deal["steamRatingCount"],
+      steam_app_id:       deal["steamAppID"],
+      thumbnail:          deal["thumb"],
+      sale_price:         deal["salePrice"],
+      normal_price:       deal["normalPrice"],
+      savings:            deal["savings"]
+    )
+end
+
+########################
+##   Free Games Seed  ##
+########################
+
+response = HTTParty.get("https://www.freetogame.com/api/games")
+games_data = JSON.parse(response.body)
+
+  games_data.each do |game|
+    game_summary = GameSummary.find_by(summary_game_title: game["title"])
+    game_summary_id = game_summary.id if game_summary
+    FreeGame.create(
+      summary_id:   game_summary_id,
+      title:        game["title"],
+      fdescription: game["description"],
+      url:          game["url"],
+      genre:        game["genre"],
+      platform:     game["platform"],
+      publisher:    game["publisher"],
+      release_date: game["releaseDate"]
+    )
+  end
 end

32365a2af3b9937cf25bde53a4b053034c4a1806
Kwanwai Pang <kpang@academic.rrc.ca>
2023-10-15 18:13:27 -0500

    _navbar.html.erb search with platform filter is now functioning.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,32 @@
 class SearchController < ApplicationController
   def search
-    @cheap_games = CheapGame.all
-    @cheap_games = @cheap_games.where("title LIKE ?", "%#{params[:search]}%") if params[:search]
-
     @free_games = FreeGame.all
-    @free_games = @free_games.where("title LIKE ?", "%#{params[:search]}%") if params[:search]
-
     @giveaways = Giveaway.all
-    @giveaways = @giveaways.where("title LIKE ?", "%#{params[:search]}%") if params[:search]
+    @cheap_games = CheapGame.none
+
+    if params[:search].present?
+      search_query = "%#{params[:search]}%"
+      @free_games = @free_games.where("title LIKE ?", search_query)
+      @giveaways = @giveaways.where("title LIKE ?", search_query)
+    end
+
+    if params[:platform].present? && params[:platform] != "All Platforms"
+      if params[:platform] == "PC"
+        @free_games = @free_games.where("platform LIKE ? OR platform LIKE ?", "%PC (Windows)%",
+                                        "%PC%")
+        @giveaways = @giveaways.where("platforms LIKE ? OR platforms LIKE ?", "%PC (Windows)%",
+                                      "%PC%")
+      else
+        @free_games = @free_games.where("platform LIKE ?", "%#{params[:platform]}")
+        @giveaways = @giveaways.where("platforms LIKE ?", "%#{params[:platform]}")
+      end
+    else
+      @cheap_games = CheapGame.all
+
+      if params[:search].present?
+        search_query = "%#{params[:search]}%"
+        @cheap_games = @cheap_games.where("title LIKE ?", search_query)
+      end
+    end
   end
 end
